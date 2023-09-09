9150bbc966b533b308421046a8a84d30c26c3671
Uttej uv <sushprav11@gmail.com>
2023-09-09 15:16:50 +0530

    sample

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "sample" {
   ami                     = "ami-01717a867ca32066a"
   instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["aws_security_group.allow_all.id"]
+  
 }
 
 # This will be executed on the top of the machine once it's created
