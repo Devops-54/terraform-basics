d415bb6f94b89614217d198a4eaa8f6bce560657
Uttej uv <sushprav11@gmail.com>
2023-09-09 14:23:38 +0530

    sample

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "web" {
-  ami           = "ami-01717a867ca32066a"
-  instance_type = "t3.nano"
+resource "aws_instance" "app_server" {
+  ami                     = "ami-01717a867ca32066a"
+  instance_type           = "t3.nano"
+  vpc_security_group_ids  = ["aws_security_group.allow_all.id"]
   
   tags = {
     Name = "TerraformServer"
