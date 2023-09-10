07f5904ab098a6cffd47adc0de091314c07d4a02
Uttej uv <sushprav11@gmail.com>
2023-09-10 18:33:38 +0530

    sample

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,8 +1,9 @@
 # Creates EC2
 resource "aws_instance" "sample" {
   ami                     = "ami-01717a867ca32066a"
-  instance_type           = "t3.micro"
-  
+  instance_type           = "t3.nano"
+  vpc_security_group_ids  = [var.sg]
 }
 
-# This will be executed on the top of the machine once it's created
+variable "sg" {}   # step 3 : Now to use this info, declare an empty varianble and user it ( in root module, we have declared " sg = var.sgid )
+
