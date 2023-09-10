32f7b9a6dbdc271ec79889d6ebaf306afcc7c068
Uttej uv <sushprav11@gmail.com>
2023-09-10 21:05:34 +0530

    sample

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,6 +1,6 @@
 # Creates EC2
 resource "aws_instance" "sample" {
-  ami                     = "data.aws_ami.image.id"
+  ami                     = data.aws_ami.image.id
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [var.sg]
 }
