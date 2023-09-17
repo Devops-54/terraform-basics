59bf6f4bfef20203ea5e94686c7f6c2ff70b8703
Uttej uv <sushprav11@gmail.com>
2023-09-17 14:38:53 +0530

    sample

--- 06-modules/ec2/data.tf
+++ 06-modules/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "lab-ami-ansible2"
+  name_regex       = "lab-ami-ansible2"    # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
   owners           = ["self"]     
 }
