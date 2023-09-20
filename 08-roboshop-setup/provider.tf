1043f99e3fe88261b331fdec980db3253ef22468
Uttej uv <sushprav11@gmail.com>
2023-09-20 15:35:42 +0530

    sample

--- 08-roboshop-setup/provider.tf
+++ 08-roboshop-setup/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "b54-tf-remote-state"
+    bucket         = "dev54-tf-remote-state"
     key            = "robot/terraform.tfstate"     # This is like the folder path that you prefer giving
     region         = "us-east-1"
   }
